dd02d62765a60a4d85ff8c80b87072963f9c7dc2
wldbf00129 <wldbd0713@gmail.com>
2025-09-01 12:50:03 +0900

    Add files via upload

---
+++ index.py
@@ -0,0 +1,15 @@
+#문자열 인덱싱하기
+
+a="Life is too short, You need Python"
+
+print(len(a))
+print(a[5])
+print(a[19])
+print(a[0]+a[19]+a[28])
+print(a[12:17])
+print(a[8:11])
+print(a[28:35])
+print(a[19:])
+print(a[:17])
+print(a[19:-7])
+print(a[:-17])
