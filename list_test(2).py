38cadbd3a5a145f4a63fab60777bb3322395b51c
wldbf00129 <wldbd0713@gmail.com>
2025-09-08 12:44:56 +0900

    Add files via upload

---
+++ list_test(2).py
@@ -0,0 +1,75 @@
+
+"""
+score=()
+print(score)
+print(type(score))
+score=[90,2,78.6,89.5,67.2,80.3,99,5,52.8]
+
+score.sort()
+print(score)
+
+#score를  역순으로  정렬하세요
+     
+h=[5,4,9,2,7,3]
+h.sort()
+h.reverse()
+print(h)
+#h.sort(reverse=True)
+
+
+fruit=["사과","포도","파인애플","사인머스켓","복숭아"]
+
+fruit.remove("파인애플")
+fruit.append("망고")
+fruit.pop(0)
+fruit.insert(2,"딸기")
+
+print(fruit)
+
+
+name_list = ["김건영","김의준","김진성","남명진","박한올"]
+print(name_list)
+name_list[2]="김하성"
+print(name_list)
+name_list[4]="백지율"
+print(name_list)
+
+
+
+print(name_list[0:2])
+print(name_list[1:4])
+print(name_list[0:5:2])
+print(name_list[1:4:2])
+
+print("남명진" in  name_list)
+
+h=[5,4,9,2,7,3]
+print(7 in h)
+print(8 in h)
+print(9 not in h)
+"""
+
+import random
+"""
+x= random. random
+print(x)
+
+x=[1,2,3,4,5,6,7,8,9]
+y= random.choice(x)
+print(y)
+
+random.shuffle(x)
+print(x)
+
+z=random.sample(x)
+print(x)
+"""
+
+x=random.randint(1,18)
+print(x)
+x=random.randint(1,18)
+print(x)
+x=random.randint(1,18)
+print(x)
+
+
