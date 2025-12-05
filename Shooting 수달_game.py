7cd624578be822ef3369dc74ce7d5fae69bcdef4
wldbf00129 <wldbd0713@gmail.com>
2025-12-05 12:50:15 +0900

    Add files via upload

---
+++ Shooting 수달_game.py
@@ -0,0 +1,163 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==================================== RESTART: C:\Users\USER\Desktop\Shooting_game.py ====================================
+pygame 2.6.0 (SDL 2.28.4, Python 3.12.2)
+Hello from the pygame community. https://www.pygame.org/contribute.html
+
+========================== RESTART: C:\Users\USER\Desktop\Shooting_game.py =========================
+pygame 2.6.0 (SDL 2.28.4, Python 3.12.2)
+Hello from the pygame community. https://www.pygame.org/contribute.html
+
+========================== RESTART: C:\Users\USER\Desktop\Shooting_game.py =========================
+pygame 2.6.0 (SDL 2.28.4, Python 3.12.2)
+Hello from the pygame community. https://www.pygame.org/contribute.html
+
+========================== RESTART: C:\Users\USER\Desktop\Shooting_game.py =========================
+pygame 2.6.0 (SDL 2.28.4, Python 3.12.2)
+Hello from the pygame community. https://www.pygame.org/contribute.html
+
+========================== RESTART: C:\Users\USER\Desktop\Shooting_game.py =========================
+pygame 2.6.0 (SDL 2.28.4, Python 3.12.2)
+Hello from the pygame community. https://www.pygame.org/contribute.html
+
+========================== RESTART: C:\Users\USER\Desktop\Shooting_game.py =========================
+pygame 2.6.0 (SDL 2.28.4, Python 3.12.2)
+Hello from the pygame community. https://www.pygame.org/contribute.html
+Traceback (most recent call last):
+  File "C:\Users\USER\Desktop\Shooting_game.py", line 27, in <module>
+    pygame.mixer.musci.load("images/bgm.wav")#배경음악 로드
+AttributeError: module 'pygame.mixer' has no attribute 'musci'. Did you mean: 'music'?
+
+========================== RESTART: C:\Users\USER\Desktop\Shooting_game.py =========================
+pygame 2.6.0 (SDL 2.28.4, Python 3.12.2)
+Hello from the pygame community. https://www.pygame.org/contribute.html
+
+========================== RESTART: C:\Users\USER\Desktop\Shooting_game.py =========================
+pygame 2.6.0 (SDL 2.28.4, Python 3.12.2)
+Hello from the pygame community. https://www.pygame.org/contribute.html
+
+========================== RESTART: C:\Users\USER\Desktop\Shooting_game.py =========================
+pygame 2.6.0 (SDL 2.28.4, Python 3.12.2)
+Hello from the pygame community. https://www.pygame.org/contribute.html
+Traceback (most recent call last):
+  File "C:\Users\USER\Desktop\Shooting_game.py", line 55, in <module>
+    p,pw,ph = img_load("수달")
+  File "C:\Users\USER\Desktop\Shooting_game.py", line 49, in img_load
+    img = pygame.image.load("images/"+str(obj)+".png")
+FileNotFoundError: No file 'images/수달.png' found in working directory 'C:\Users\USER\Desktop'.
+
+========================== RESTART: C:\Users\USER\Desktop\Shooting_game.py =========================
+pygame 2.6.0 (SDL 2.28.4, Python 3.12.2)
+Hello from the pygame community. https://www.pygame.org/contribute.html
+Traceback (most recent call last):
+  File "C:\Users\USER\Desktop\Shooting_game.py", line 55, in <module>
+    p,pw,ph = img_load("tnekf")
+  File "C:\Users\USER\Desktop\Shooting_game.py", line 49, in img_load
+    img = pygame.image.load("images/"+str(obj)+".png")
+FileNotFoundError: No file 'images/tnekf.png' found in working directory 'C:\Users\USER\Desktop'.
+
+========================== RESTART: C:\Users\USER\Desktop\Shooting_game.py =========================
+pygame 2.6.0 (SDL 2.28.4, Python 3.12.2)
+Hello from the pygame community. https://www.pygame.org/contribute.html
+Traceback (most recent call last):
+  File "C:\Users\USER\Desktop\Shooting_game.py", line 55, in <module>
+    p,pw,ph = img_load("tnekf.png")
+  File "C:\Users\USER\Desktop\Shooting_game.py", line 49, in img_load
+    img = pygame.image.load("images/"+str(obj)+".png")
+FileNotFoundError: No file 'images/tnekf.png.png' found in working directory 'C:\Users\USER\Desktop'.
+
+========================== RESTART: C:\Users\USER\Desktop\Shooting_game.py =========================
+pygame 2.6.0 (SDL 2.28.4, Python 3.12.2)
+Hello from the pygame community. https://www.pygame.org/contribute.html
+Traceback (most recent call last):
+  File "C:\Users\USER\Desktop\Shooting_game.py", line 55, in <module>
+    p,pw,ph = img_load("tnekf")
+  File "C:\Users\USER\Desktop\Shooting_game.py", line 49, in img_load
+    img = pygame.image.load("images/"+str(obj)+".png")
+FileNotFoundError: No file 'images/tnekf.png' found in working directory 'C:\Users\USER\Desktop'.
+
+========================== RESTART: C:\Users\USER\Desktop\Shooting_game.py =========================
+pygame 2.6.0 (SDL 2.28.4, Python 3.12.2)
+Hello from the pygame community. https://www.pygame.org/contribute.html
+
+========================== RESTART: C:\Users\USER\Desktop\Shooting_game.py =========================
+pygame 2.6.0 (SDL 2.28.4, Python 3.12.2)
+Hello from the pygame community. https://www.pygame.org/contribute.html
+
+========================== RESTART: C:\Users\USER\Desktop\Shooting_game.py =========================
+pygame 2.6.0 (SDL 2.28.4, Python 3.12.2)
+Hello from the pygame community. https://www.pygame.org/contribute.html
+
+========================== RESTART: C:\Users\USER\Desktop\Shooting_game.py =========================
+pygame 2.6.0 (SDL 2.28.4, Python 3.12.2)
+Hello from the pygame community. https://www.pygame.org/contribute.html
+Traceback (most recent call last):
+  File "C:\Users\USER\Desktop\Shooting_game.py", line 167, in <module>
+    gameover()
+  File "C:\Users\USER\Desktop\Shooting_game.py", line 30, in gameover
+    paygame.display.update()
+NameError: name 'paygame' is not defined. Did you mean: 'pygame'?
+
+========================== RESTART: C:\Users\USER\Desktop\Shooting_game.py =========================
+pygame 2.6.0 (SDL 2.28.4, Python 3.12.2)
+Hello from the pygame community. https://www.pygame.org/contribute.html
+
+========================== RESTART: C:\Users\USER\Desktop\Shooting_game.py =========================
+pygame 2.6.0 (SDL 2.28.4, Python 3.12.2)
+Hello from the pygame community. https://www.pygame.org/contribute.html
+
+========================== RESTART: C:\Users\USER\Desktop\Shooting_game.py =========================
+pygame 2.6.0 (SDL 2.28.4, Python 3.12.2)
+Hello from the pygame community. https://www.pygame.org/contribute.html
+ 
+
+========================== RESTART: C:\Users\USER\Desktop\Shooting_game.py =========================
+pygame 2.6.0 (SDL 2.28.4, Python 3.12.2)
+Hello from the pygame community. https://www.pygame.org/contribute.html
+
+========================== RESTART: C:\Users\USER\Desktop\Shooting_game.py =========================
+pygame 2.6.0 (SDL 2.28.4, Python 3.12.2)
+Hello from the pygame community. https://www.pygame.org/contribute.html
+
+
+========================== RESTART: C:\Users\USER\Desktop\Shooting_game.py =========================
+pygame 2.6.0 (SDL 2.28.4, Python 3.12.2)
+Hello from the pygame community. https://www.pygame.org/contribute.html
+>>> 
+========================== RESTART: C:\Users\USER\Desktop\Shooting_game.py =========================
+pygame 2.6.0 (SDL 2.28.4, Python 3.12.2)
+Hello from the pygame community. https://www.pygame.org/contribute.html
+>>> 
+========================== RESTART: C:\Users\USER\Desktop\Shooting_game.py =========================
+pygame 2.6.0 (SDL 2.28.4, Python 3.12.2)
+Hello from the pygame community. https://www.pygame.org/contribute.html
+>>> 
+========================== RESTART: C:\Users\USER\Desktop\Shooting_game.py =========================
+pygame 2.6.0 (SDL 2.28.4, Python 3.12.2)
+Hello from the pygame community. https://www.pygame.org/contribute.html
+
+>>> 
+========================== RESTART: C:\Users\USER\Desktop\Shooting_game.py =========================
+pygame 2.6.0 (SDL 2.28.4, Python 3.12.2)
+Hello from the pygame community. https://www.pygame.org/contribute.html
+>>> 
+========================== RESTART: C:\Users\USER\Desktop\Shooting_game.py =========================
+pygame 2.6.0 (SDL 2.28.4, Python 3.12.2)
+Hello from the pygame community. https://www.pygame.org/contribute.html
+>>> 
+========================== RESTART: C:\Users\USER\Desktop\Shooting_game.py =========================
+pygame 2.6.0 (SDL 2.28.4, Python 3.12.2)
+Hello from the pygame community. https://www.pygame.org/contribute.html
+>>> 
+========================== RESTART: C:\Users\USER\Desktop\Shooting_game.py =========================
+pygame 2.6.0 (SDL 2.28.4, Python 3.12.2)
+Hello from the pygame community. https://www.pygame.org/contribute.html
+>>> 
+========================== RESTART: C:\Users\USER\Desktop\Shooting_game.py =========================
+pygame 2.6.0 (SDL 2.28.4, Python 3.12.2)
+Hello from the pygame community. https://www.pygame.org/contribute.html
+>>> 
+========================== RESTART: C:\Users\USER\Desktop\Shooting_game.py =========================
+pygame 2.6.0 (SDL 2.28.4, Python 3.12.2)
+Hello from the pygame community. https://www.pygame.org/contribute.html
